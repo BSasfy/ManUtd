1353fbc5ef59cb8e1055ea79764cea15b531b7e3
Matt Adams <mattadams@Matts-MBP.home>
2021-11-25 13:58:08 +0000

    Update method to stringify data

--- src/getToken.js
+++ src/getToken.js
@@ -1,32 +1,29 @@
-import Axios from 'axios'
+import axios from 'axios';
+import * as qs from 'qs';
 
-const data = {
-  grant_type: 'client_credentials',
-  client_id: '2hchrq9nuvbg7rojh527smv7fq', 
-  client_secret: '9i3qjkpjd5qvamq9lp7l2lvo1alfecobujrgmrfqt13bst0s6dg',
-};
+var data = qs.stringify({
+	client_id: '33m37fu1hih5u52b8gmb1as4c6',
+	client_secret: '1s2ebtssnvirc1kclkb9gve4kitiqioh00ik9lrb8to8sj5mit9j',
+	grant_type: 'client_credentials'
+});
 
-const reqConfig = {
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
+var config = {
+	method: 'post',
+	url: 'https://manutd.auth.eu-west-1.amazoncognito.com/token',
+	headers: {
+		'Content-Type': 'application/x-www-form-urlencoded'
+	},
+	data: data
 };
 
 async function getToken() {
-  console.log("Hello World");
-  
-  try {
-    const result = await Axios.post('https://manutd.auth.eu-west-2.amazoncognito.com/token', data, reqConfig);
-
-    console.log ('result>>>>>', result);
-
-  }
-  catch (error){
-    console.log('error>>>>')
-  }
-
-
+	axios(config)
+		.then((response) => {
+			console.log(JSON.stringify(response.data));
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
 }
 
-export default getToken
-
+export default getToken;
